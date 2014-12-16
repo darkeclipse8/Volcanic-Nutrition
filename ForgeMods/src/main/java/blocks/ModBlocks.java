0b477727e15953f4d91cccc1523d6deeddb1703d
Benjamin Ngo <bengo@sbcglobal.net>
2014-12-16 10:21:19 -0800

    More updates
    SHould've added potentBaked potatoe, melon block and more.

--- ForgeMods/src/main/java/blocks/ModBlocks.java
+++ ForgeMods/src/main/java/blocks/ModBlocks.java
@@ -6,9 +6,11 @@ public final class ModBlocks {
 	
 	public static Block potentDirtBlock;
 	public static Block potentFarmlandDirtBlock;
+	public static Block potentMelonBlock;
 	
 	public static void init() {
 		potentDirtBlock = new potentDirtBlock();
 		potentFarmlandDirtBlock = new potentFarmlandDirtBlock();
+		potentMelonBlock = new potentMelonBlock();
 	}
 }
--- ForgeMods/src/main/java/foods/VolcanicFood.java
+++ ForgeMods/src/main/java/foods/VolcanicFood.java
@@ -1,7 +1,9 @@
 package foods;
 
+import lib.Constants;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 
@@ -10,15 +12,29 @@ public final class VolcanicFood {
 	public static ItemFood powerCarrot;
 	public static ItemFood powerMelon;
 	public static ItemFood powerPotatoe;
+	public static ItemFood potentBakedPotatoe;
 	
 	public static void init() {
-		powerCarrot = new powerCarrot(1.0f, false);
-		powerMelon = new powerMelon(1.0f, false);
-		powerPotatoe = new powerPotatoe(1.0f, false);
+		powerCarrot = new powerCarrot(0.72f, false);
+		powerMelon = new powerMelon(0.24f, false);
+		powerPotatoe = new powerPotatoe(0.12f, false);
+		potentBakedPotatoe = new potentBakedPotatoe(0.8f, false);
 		createRecipes();
 	}
 	
 	public static void createRecipes() {
+		
+		//String nameOfItem = "potentBakedPotatoe";
+		//Item potentBakedPotatoe = new ItemFood(7, 0.6F, false).setUnlocalizedName("potatoBaked").setTextureName(Constants.MODID + ":" + nameOfItem);
+		GameRegistry.addSmelting(powerPotatoe, new ItemStack(potentBakedPotatoe), 4);
+		
+		// Recipe for melons to create melon block
+		GameRegistry.addRecipe(new ItemStack(new blocks.ModBlocks().potentMelonBlock), new Object[] {
+			"AAA",
+			"AAA",
+			"AAA",
+			'A', powerMelon
+		});
 		
 		// Recipe for two carrots to make the power carrot
 		GameRegistry.addRecipe(new ItemStack(powerCarrot), new Object[] {
